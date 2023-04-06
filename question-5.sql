6db0c10f1a4fec33b4ca195a7bc968b35061dcda
ohnoaa <121905766+ohnoaa@users.noreply.github.com>
2023-04-06 13:34:06 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,4 +1,4 @@
--- Question 5: What is the first name of everybody who has a last name similar to &quot;Smith&quot; (that is to say, their last name starts with Smith)?
+-- Question 5: What is the first name of everybody who has a last name similar to Smith (that is to say, their last name starts with Smith)?
 SELECT first_name
 FROM persons
 WHERE last_name LIKE 'Smith%';
