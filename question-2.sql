c0ee419c829cb3ecbeb5608146a4f226796d0085
ohnoaa <121905766+ohnoaa@users.noreply.github.com>
2023-04-06 19:38:28 -0700

    Update question-2.sql

--- question-2.sql
+++ question-2.sql
@@ -1,5 +1,5 @@
 -- Question 2: What are the full name(s) (last and first, in sorted order by last name) who have an age less than 30 (meaning age 29 and down)?
 SELECT last_name, first_name
 FROM persons
-WHERE age &lt; 30
+WHERE age < 30
 ORDER BY last_name ASC;
